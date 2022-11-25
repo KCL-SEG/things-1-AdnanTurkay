d9a97ac401d5a595eec8cb27b3abd2d4ebec3125
AdnanTurkay <adntrky@gmail.com>
2022-11-25 06:21:50 +0000

    Initial solution for "Exercise 2"

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=20)
+    quantity = models.IntegerField()
